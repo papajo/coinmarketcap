0d0e678b0ba7abaa6e3bf347195faebd297496ae
papajo <joshipv2@gmail.com>
2018-07-04 15:40:33 -0400

    finish /ticker and /specific EPs

--- coincap_ticker.py
+++ coincap_ticker.py
@@ -1,23 +1,62 @@
 import json
 import requests
 
-ticker_url = 'https://api.coinmarketcap.com/v2/ticker?structure=array'
+while True:
+    ticker_url = 'https://api.coinmarketcap.com/v2/ticker?structure=array'
 
-limit = 100
-start = 1
-sort = 'id'
-convert = 'USD'
+    limit = 100
+    start = 1
+    sort = 'rank'
+    convert = 'USD'
 
-choice = input("Do you want to pass any custom parameters? (y/n): ")
+    choice = input("Do you want to pass any custom parameters? (y/n): ")
 
-if choice == "y":
-    limit = input("What is the custom limit? :")
-    start = input("What is the start number? :")
-    sort = input("What do you want to sort by? : ")
-    convert = input("What is your local currency? :")
+    if choice == "y":
+        limit = input("What is the custom limit? :")
+        start = input("What is the start number? :")
+        sort = input("What do you want to sort by? : ")
+        convert = input("What is your local currency? :")
 
-ticker_url += '&limit=' + limit + '&sort=' + sort + '&start=' + start + '&convert' + convert
-request = requests.get(ticker_url)
-results = request.json()
+    ticker_url += '&limit=' + str(limit) + '&sort=' + sort + '&start=' + str(start) + '&convert' + convert
+    request = requests.get(ticker_url)
+    results = request.json()
 
-print(json.dumps(results, sort_keys=True, indent=4))
+    #print(json.dumps(results, sort_keys=True, indent=4))
+
+    data = results['data']
+
+    for currency in data:
+        rank = currency['rank']
+        name = currency['name']
+        symbol = currency['symbol']
+
+        circulating_supply = int(currency['circulating_supply'])
+        total_supply = int(currency['total_supply'])
+
+        quotes = currency['quotes'][convert]
+        market_cap = quotes['market_cap']
+        hour_change = quotes['percent_change_1h']
+        day_change = quotes['percent_change_24h']
+        week_change = quotes['percent_change_7d']
+        price = quotes['price']
+        volume = quotes['volume_24h']
+
+        volume_string = '{:,}'.format(volume)
+        market_cap_string = '{:,}'.format(market_cap)
+        circulating_supply_string = '{:,}'.format(circulating_supply)
+        total_supply_string = '{:,}'.format(total_supply)
+
+        print(str(rank) + ': ' + name + ' (' + symbol +')')
+        print('Market Cap: \t\t$' + market_cap_string)
+        print('Price: \t\t\t$' + str(price))
+        print('24hr Volume: \t\t$' + volume_string)
+        print('Hour Change: \t\t' + str(hour_change) + '%')
+        print('Day Change: \t\t' + str(day_change) + '%')
+        print('Week Change: \t\t' + str(week_change) + '%')
+        print('Total Supply \t\t' + total_supply_string)
+        print('Circulating Supply \t' + circulating_supply_string)
+        print('Percentage of coins in circulation ' + str(int(circulating_supply / total_supply * 100)))
+        print()
+choice = input('Run Again? : y/n')
+if choice == 'n':
+    break
